afb8976bc43e6c0f008415622be5f3394e485fd4
Martin Krsak <xkrsak@fi.muni.cz>
2022-03-30 16:59:49 +0200

    Extract formating from calculatings

--- csharp/TheatricalPlayersRefactoringKata/StatementPrinter.cs
+++ csharp/TheatricalPlayersRefactoringKata/StatementPrinter.cs
@@ -8,6 +8,7 @@ namespace TheatricalPlayersRefactoringKata
     {
         public string Print(Invoice invoice, Dictionary<string, Play> plays)
         {
+            var perfomanceAmount = new Dictionary<string, int>();
             var totalAmount = 0;
             var volumeCredits = 0;
             var result = string.Format("Statement for {0}\n", invoice.Customer);
@@ -19,11 +20,19 @@ namespace TheatricalPlayersRefactoringKata
                 var thisAmount = 0;
                 thisAmount = ComputeAmount(play, perf);
                 volumeCredits += CalculateVolumeCredits(perf, play);
-
-                // print line for this order
-                result += string.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(thisAmount / 100), perf.Audience);
+                
+                perfomanceAmount.Add(perf.PlayID, thisAmount);
                 totalAmount += thisAmount;
             }
+
+            foreach (var perf in invoice.Performances)
+            {
+                var play = plays[perf.PlayID];
+                var thisAmount = perfomanceAmount[perf.PlayID];
+                // print line for this order
+                result += string.Format(cultureInfo, "  {0}: {1:C} ({2} seats)\n", play.Name, Convert.ToDecimal(thisAmount / 100), perf.Audience);
+            }
+            
             result += string.Format(cultureInfo, "Amount owed is {0:C}\n", Convert.ToDecimal(totalAmount / 100));
             result += string.Format("You earned {0} credits\n", volumeCredits);
             return result;
